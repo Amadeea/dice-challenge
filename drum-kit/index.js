341beb68dd17299cda3cec901f69c1dadc51e6d5
Amadeea <amadeakristina@gmail.com>
2025-05-10 11:42:56 +0700

    add button pressed animation

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -10,28 +10,47 @@ var audioKick = new Audio("sounds/kick-bass.mp3");
 
 for (let i = 0; i < numberOfDrums; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    switch (this.textContent) {
-      case "w":
-        audioTom1.play();
-        break;
-      case "a":
-        audioTom2.play();
-        break;
-      case "s":
-        audioTom3.play();
-        break;
-      case "d":
-        audioTom4.play();
-        break;
-      case "j":
-        audioSnare.play();
-        break;
-      case "k":
-        audioCrash.play();
-        break;
-      case "l":
-        audioKick.play();
-        break;
-    }
+    buttonAnimation(this.textContent);
+    makeSound(this.textContent);
   });
+}
+
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      audioTom1.play();
+      break;
+    case "a":
+      audioTom2.play();
+      break;
+    case "s":
+      audioTom3.play();
+      break;
+    case "d":
+      audioTom4.play();
+      break;
+    case "j":
+      audioSnare.play();
+      break;
+    case "k":
+      audioCrash.play();
+      break;
+    case "l":
+      audioKick.play();
+      break;
+  }
+}
+
+function buttonAnimation(key) {
+  var targetButton = document.querySelector("." + key);
+
+  targetButton.classList.add("pressed");
+  setTimeout(function () {
+    targetButton.classList.remove("pressed");
+  }, 100);
 }
